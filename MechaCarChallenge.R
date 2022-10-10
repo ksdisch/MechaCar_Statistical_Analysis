c21003b80c0ae7d6a9083b11162161b66fcea922
ksdisch <kyledisch@icloud.com>
2022-10-10 14:15:54 -0500

    t tests

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -9,3 +9,9 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 
 # summary to determine p and r-squared values
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD + mpg,data = mecha_mpg))
+
+suspension_table <- read.csv(file = 'Suspension_Coil.csv',check.names = F,stringsAsFactors = F)
+
+total_summary <- suspension_table %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+
+lot_summary <- suspension_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep')
