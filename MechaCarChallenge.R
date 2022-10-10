72dc7b698054afcc033087970df22dde1fe2a4aa
ksdisch <kyledisch@icloud.com>
2022-10-10 15:10:12 -0500

    updated mechacarchallenge.r

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -15,3 +15,18 @@ suspension_table <- read.csv(file = 'Suspension_Coil.csv',check.names = F,string
 total_summary <- suspension_table %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
 
 lot_summary <- suspension_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep')
+
+psi_t_test <- t.test(suspension_table$PSI, mu=1500)
+
+lot1 <- suspension_table[suspension_table$Manufacturing_Lot == "Lot1",]
+
+lot2 <- suspension_table[suspension_table$Manufacturing_Lot == "Lot2",]
+
+lot3 <- suspension_table[suspension_table$Manufacturing_Lot == "Lot3",]
+
+lot1_t_test <- t.test(x=lot1$PSI, mu=1500)
+
+lot2_t_test <- t.test(x=lot2$PSI, mu=1500)
+
+lot3_t_test <- t.test(x=lot3$PSI, mu=1500)
+
